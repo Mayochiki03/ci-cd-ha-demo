119d3f4b81606e543dc6b1122249c892685f4151
T-NATTHAPOL\Apisorn <654234013@parichat.skru.ac.th>
2026-01-30 14:48:03 +0700

    server

--- server.js
+++ server.js
@@ -1,34 +1,24 @@
 const http = require("http");
-const fs = require("fs");
-const path = require("path");
 const os = require("os");
 
 const PORT = 3000;
 const HOST = "0.0.0.0";
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    const html = fs.readFileSync(path.join(__dirname, "public/index.html"));
+  try {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(html);
-    return;
+    res.end(`
+      <html>
+        <body style="font-family:sans-serif;text-align:center;margin-top:50px">
+          <h1>CI/CD + Load Balance Demo</h1>
+          <p>Response from: <b>${os.hostname()}</b></p>
+        </body>
+      </html>
+    `);
+  } catch (err) {
+    res.writeHead(500);
+    res.end("Internal Server Error");
   }
-
-  if (req.url === "/style.css") {
-    const css = fs.readFileSync(path.join(__dirname, "public/style.css"));
-    res.writeHead(200, { "Content-Type": "text/css" });
-    res.end(css);
-    return;
-  }
-
-  if (req.url === "/api/server") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(`Host: ${os.hostname()}`);
-    return;
-  }
-
-  res.writeHead(404);
-  res.end("Not Found");
 });
 
 server.listen(PORT, HOST, () => {
