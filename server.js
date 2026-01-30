a345a7b5c829fae484d7f895d54a4cfa40f05223
T-NATTHAPOL\Apisorn <654234013@parichat.skru.ac.th>
2026-01-30 14:33:40 +0700

    add html web

--- server.js
+++ server.js
@@ -1,16 +1,36 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const os = require("os");
 
 const PORT = 3000;
 const HOST = "0.0.0.0";
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
+  if (req.url === "/") {
+    const html = fs.readFileSync(path.join(__dirname, "public/index.html"));
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(html);
+    return;
+  }
 
-  const message = "Hello from FIXED version update \n";
+  if (req.url === "/style.css") {
+    const css = fs.readFileSync(path.join(__dirname, "public/style.css"));
+    res.writeHead(200, { "Content-Type": "text/css" });
+    res.end(css);
+    return;
+  }
 
-  res.end(message);
+  if (req.url === "/api/server") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(`Host: ${os.hostname()}`);
+    return;
+  }
+
+  res.writeHead(404);
+  res.end("Not Found");
 });
 
 server.listen(PORT, HOST, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on ${HOST}:${PORT}`);
 });
