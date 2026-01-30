b3bd3df62c0a3960070dab81ccce92f5ad95294c
T-NATTHAPOL\Apisorn <654234013@parichat.skru.ac.th>
2026-01-30 13:27:38 +0700

    test bug

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const http = require('http');
+const http = require("http");
 
 const PORT = 3000;
-const HOST = '0.0.0.0';
+const HOST = "0.0.0.0";
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from CI/CD build - Version V2\n');
+  res.writeHead(200, { "Content-Type": "text/plain" });
+
+  message = "Hello from BUG version\n"; //ไม่มี const / let
+  res.end(message);
 });
 
 server.listen(PORT, HOST, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
